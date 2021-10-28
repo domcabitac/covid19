7dac4c4179e0d5665fa5a968ab6f8935570ef3ac
Dominic Cabitac <dominic.cabitac@uoit.net>
2021-10-28 13:12:00 -0400

    Made numbers more readable

--- src/app/components/dashboard-card/dashboard-card.component.ts
+++ src/app/components/dashboard-card/dashboard-card.component.ts
@@ -175,5 +175,29 @@ export class DashboardCardComponent implements OnInit {
 
     this.updateData(x);
   }
+ convertNumber(x) {
+    if (isNaN(x)) return x;
+
+    if (x < 9999) {
+      return x;
+    }
+
+    if (x < 1000000) {
+      return Math.round(x / 1000) + "K";
+    }
+    if (x < 10000000) {
+      return (x / 1000000).toFixed(3) + "M";
+    }
+
+    if (x < 1000000000) {
+      return (x / 1000000).toFixed(2) + "M";
+    }
+
+    if (x < 1000000000000) {
+      return Math.round((x / 1000000000)) + "B";
+    }
+
+    return "1T+";
+  }
 
 }
